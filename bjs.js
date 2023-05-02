5d175bafc9ea5e4ec4e8da3bbe62d726433e5163
Vercte <lopstedener@gmail.com>
2023-05-02 12:54:02 -0700

    make it faster through epic optimizations

--- bjs.js
+++ bjs.js
@@ -1,4 +1,41 @@
 window.bjs = function(s) {
-  let func = [101, 118, 97, 108];
-  return window[(func.map(n => String.fromCharCode(n))).join("")](s);
+  let m = (a) => a.map(n => String.fromCharCode(n)).join("");
+  let e = [101,118,
+           97, 108];
+  let w = [119,105,
+           110,100,
+           111,119];
+  let d = [100,111,
+           99, 117,
+           109,101,
+           110,116];
+  let ce = [99, 114,
+            101,97,
+            116,101,
+            69, 108,
+            101,109,
+            101,110,
+              116];
+  let ic = [105,102,
+            114, 97,
+            109,101];
+  let is = window[m(w)][m(d)][m(ce)](m(ic));
+  let iw = [99, 111,
+            110,116,
+            101,110,
+            116, 87,
+            105,110,
+            100,111,
+              119];
+  let bd = [98, 111,
+            100,121];
+  let ap = [97, 112,
+            112,101,
+            110,100];
+  window[m(w)][m(d)][m(bd)][m(ap)](is);
+  return is
+          [m(iw)]
+          [m(w)]
+          [m(e)]
+          (s);
 }
