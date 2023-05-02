14e3f9512f25d62d8f8ba4d993b6f8ce81f1c886
Vercte <lopstedener@gmail.com>
2023-05-02 12:19:02 -0700

    Update bjs.js
    fig bux which caused issues

--- bjs.js
+++ bjs.js
@@ -1,4 +1,4 @@
 window.bjs = function(s) {
   let func = [101, 118, 97, 108];
-  return window[(func.map(n => String.fromCharCode)).join("")](s);
+  return window[(func.map(n => String.fromCharCode(n))).join("")](s);
 }
